3fceeecb0235b134d37bcfe82dd4c82e2680dc98
jeronimusus <j_van_dijk@hotmail.com>
2021-10-19 15:03:37 +0100

    Added Tests to Post Order endpoint

--- src/test/java/GetOrderTest.java
+++ src/test/java/GetOrderTest.java
@@ -1,10 +1,15 @@
 import functionality.endpoints.GetOrder;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GetOrderTest {
     @Test
     public void getAnOrder(){
         GetOrder order = new GetOrder();
         order.writePayload();
+
     }
 }
--- src/test/java/PostOrderTest.java
+++ src/test/java/PostOrderTest.java
@@ -3,6 +3,10 @@ import functionality.endpoints.GetOrder;
 import functionality.endpoints.PostOrder;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class PostOrderTest {
     @Test
     public void postAnOrderWithNoDetails(){
@@ -22,6 +26,11 @@ public class PostOrderTest {
     public void postAnOrderWithPizzaDetails(){
         PostOrder order = new PostOrder("Cheesy", "Curry", "Medium", 5);
         order.writePayload();
+        // Tests
+        assertThat("Cheesy", equalTo(order.getPayloadJson().get("Crust")));
+        assertThat("Curry", equalTo(order.getPayloadJson().get("Flavor")));
+        assertThat("Medium", equalTo(order.getPayloadJson().get("Size")));
+        assertThat(5, equalTo(order.getPayloadJson().get("Table_No")));
         // Now delete the order we just created to keep the DB tidy
         DeleteOrder orderToDelete = new DeleteOrder(String.valueOf(order.getOrderId()));
         orderToDelete.writePayload();
