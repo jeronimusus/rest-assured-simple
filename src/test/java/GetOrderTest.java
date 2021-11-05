9406dd3aeb07a68b1c427d643b7c5f287745208a
jeronimusus <j_van_dijk@hotmail.com>
2021-11-05 15:10:52 +0000

    Added JSON Path to LIST and to Array of type IndividualOrder

--- src/test/java/GetOrderTest.java
+++ src/test/java/GetOrderTest.java
@@ -16,12 +16,25 @@ public class GetOrderTest {
         orders.writePayload();
         System.out.println(orders.getResponse().getStatusCode());
         assertThat(200, equalTo(orders.getResponse().getStatusCode()));
-        // Add all the individual Orders to a list
+
+        // Add all order crusts to a List of type String
+        List<String> crusts = orders.getPayloadJson().getList("Crust");
+        System.out.println(crusts.get(0));
+        System.out.println(crusts.get(1));
+
+        // Add all the individual Orders to a List of type IndividualOrder
         List<IndividualOrder> allOrders = orders.getPayloadJson().getList("", IndividualOrder.class);
         for(IndividualOrder order : allOrders)
         {
             System.out.println("Order Timestamp: " + order.Timestamp);
             assertThat(order.Timestamp, matchesPattern("^^[1-9]\\d{3}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{6}$"));
+        }
+
+        // Add all the individual Orders in to an Array of type IndividualOrder
+        IndividualOrder[] arrayOfOrders = orders.getPayloadJson().getObject("", IndividualOrder[].class);
+        for(IndividualOrder order : arrayOfOrders)
+        {
+            System.out.println("Order Timestamp again " + order.Timestamp);
         }
     }
 }
